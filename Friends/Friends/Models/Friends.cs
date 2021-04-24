cef541e1c457d701dbe6daee810b0e705a4985ef
Daniela Melendres Villarreal <a2017113441@estudiantes.upsa.edu.bo>
2021-04-24 11:41:23 -0400

    Se agrego base de datos y data context

---
+++ Friends/Friends/Models/Friends.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Friends.Models
+{
+    public class Friends
+    {
+        [Key]
+        public string FriendId { get; set; }
+        [Required]
+        [StringLength(30, MinimumLength = 3)]
+        public string Name { get; set; }
+        public string NickName { get; set; }
+        [DataType(DataType.Date)]
+        public int BirthDay { get; set; }
+    }
+}
